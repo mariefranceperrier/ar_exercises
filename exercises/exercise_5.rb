353ed20bb5835d33c6538e8c46fd0854fb0f4b06
mariefranceperrier <mariefrance.perrier@gmail.com>
2024-05-01 11:53:14 -0400

    implement exercise_5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,14 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
-puts "Exercise 5"
+puts "\nExercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "Total revenue for the entire company: $#{total_revenue}"
+
+average_revenue = Store.average(:annual_revenue)
+puts "Average annual revenue for all stores: $#{average_revenue}"
+
+high_revenue_stores = Store.where('annual_revenue >= 1000000').count
+puts "Number of stores generating $1,000,000 or more in annual sales: #{high_revenue_stores}"
