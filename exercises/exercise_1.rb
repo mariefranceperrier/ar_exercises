4a9adf798d16ef963dbdee308141989ab8765dfe
mariefranceperrier <mariefrance.perrier@gmail.com>
2024-05-01 11:18:39 -0400

    add new line between exercises

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,6 @@
 require_relative '../setup'
 
-puts "Exercise 1"
+puts "\nExercise 1"
 puts "----------"
 
 Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
