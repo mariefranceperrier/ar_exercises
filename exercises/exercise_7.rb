570f857cfe0c3548eaf9397e31f1692c63467f63
mariefranceperrier <mariefrance.perrier@gmail.com>
2024-05-01 13:52:43 -0400

    implement exercise_7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -6,7 +6,22 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
-puts "Exercise 7"
+puts "\nExercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Please enter a store name:"
+store_name = gets.chomp
+
+puts "Attempting to create a store with name: #{store_name}"
+
+new_store = Store.new(name: store_name) 
+
+if new_store.save
+  puts "Store created successfully!"
+else
+  puts "Failed to create store. Error messages:"
+  new_store.errors.full_messages.each do |message|
+    puts message
+  end
+end
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+  validate :must_carry_apparel
+
+  private
+
+  def must_carry_apparel
+    unless mens_apparel || womens_apparel
+      errors.add(:base, "Store must carry at least one type of apparel")
+    end
+  end
 end
